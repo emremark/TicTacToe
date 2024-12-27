238247b18fe8382d9b9bc9a51483020bafe4aec2
emremark <mark.radicc@gmail.com>
2024-12-27 16:09:26 +0100

    Change script to work outside console with DOM
    Functions 1 and 2 are IFFE`s now. Add a function
    to control the DOM.

--- script.js
+++ script.js
@@ -1,79 +1,230 @@
-function gameBoard() {
-    const board = ["", "", "","", "", "", "", "", ""]
+const gameB = (function () {
+  let board = ["", "", "", "", "", "", "", "", ""];
 
-    const getBoard = () => board;
-  
-    const changeBoard = (index, mark) => {
-      if (board[index] === "") {
-      board[index] = mark
-      return true
-      }
+  const getBoard = () => board;
+
+  const move = (p, turn) => {
+    if (board[p] === "") {
+      board[p] = turn.name;
     }
-  
-    const check = () => {
-    if ((board[0] === "X" && board[1] === "X" && board[2] === "X") || (board[0] === "Y" && board[1] === "Y" && board[2] === "Y")) {
-        return true
-      }
-    if ((board[3] === "X" && board[4] === "X" && board[5] === "X") || (board[3] === "Y" && board[4] === "Y" && board[5] === "Y")) {
-        return true
-      }
-    if ((board[6] === "X" && board[7] === "X" && board[8] === "X") || (board[6] === "Y" && board[7] === "Y" && board[8] === "Y")) {
-        return true
-      }  
-    if ((board[0] === "X" && board[3] === "X" && board[6] === "X") || (board[0] === "Y" && board[3] === "Y" && board[6] === "Y")) {
-        return true
-      }  
-    }
-  
-    return {getBoard, changeBoard, check}
   }
 
-  function domControl(gameBoard) {
-    const elem = document.querySelectorAll(".in");
-    const board = gameBoard.getBoard();
+  return {getBoard, move}
 
-    elem.forEach((el, ind) => {
-        el.addEventListener("click", () => {
-            gameFlow.play(ind); 
-        })
+})();
+
+const gameC = (function () {
+
+  const players = [
+    {name: "X", score: 0}, 
+    {name: "O", score: 0}
+  ]
+  let turn = players[0];
+
+  const addpoint = () => {
+    turn.score += 1;
+  }
+
+  const switchTurn = () => {
+    turn = turn === players[0] ? players[1] : players[0];
+  }
+  const getTurn = () => turn;
+
+  return {addpoint, switchTurn, getTurn}
+})();
+
+function screenControler(gameB,gameC) {
+  const bdiv = document.querySelector(".gboard");
+  const next = document.querySelector(".turn")
+
+  const updateScreen = () => {
+    bdiv.textContent = "";
+
+    const brd = gameB.getBoard();
+    const trn = gameC.getTurn();
+    next.textContent = `${trn.name}'s turn`
+
+    brd.forEach((el, ind) => {
+          console.log(gameB.getBoard());
+          const btn = document.createElement("button");
+          btn.dataset.p = ind;
+          btn.innerHTML = el;
+          console.log(gameB.getBoard());
+          bdiv.appendChild(btn);
+      })
+  }
+
+  function whenClick(e) {
+    const p = e.target.dataset.p;
+    gameB.move(p, gameC.getTurn());
+    gameC.switchTurn();
+    updateScreen();
+  }
+  bdiv.addEventListener("click", whenClick);
+
+  updateScreen();
+}
+
+screenControler(gameB, gameC);
+
+
+/*function gameBoard() {
+  let board = ["", "", "", "", "", "", "", "", ""];
+
+  const getBoard = () => board;
+
+  const move = (p, turn) => {
+    if (board[p] === "") {
+      board[p] = turn.name;
+    }
+  }
+
+  return {getBoard, move}
+}
+
+function gameControler() {
+  const board = gameBoard();
+
+  const players = [
+    {name: "X", score: 0}, 
+    {name: "O", score: 0}
+  ]
+  let turn = players[0];
+
+  const addpoint = () => {
+    turn.score += 1;
+  }
+
+  const getTurnName = () => turn.name;
+
+  const switchTurn = () => {
+    turn = turn === players[0] ? players[1] : players[0];
+  }
+  const getTurn = () => turn;
+
+  return {addpoint, getTurnName, switchTurn, getTurn}
+}
+
+function screenControler() {
+  const game = gameControler();
+  const board = gameBoard();
+  const bdiv = document.querySelector(".gboard");
+  const next = document.querySelector(".turn")
+
+  const updateScreen = () => {
+    bdiv.textContent = "";
+
+    const brd = board.getBoard();
+    const trn = game.getTurn();
+    next.textContent = `${trn.name}'s turn`
+    debugger
+
+    brd.forEach((el, ind) => {
+          debugger
+          const btn = document.createElement("button");
+          btn.dataset.p = ind;
+          btn.innerHTML = el;
+          bdiv.appendChild(btn);
+      })
+  }
+
+  function whenClick(e) {
+    const p = e.target.dataset.p;
+    debugger
+    board.move(p, game.getTurnName());
+    debugger
+    game.switchTurn();
+    debugger
+    updateScreen();
+    debugger
+  }
+  bdiv.addEventListener("click", whenClick);
+
+  updateScreen();
+}
+
+screenControler();
+
+
+
+
+
+/*function gameBoard() {
+  const board = [
+      ["", "", ""], 
+      ["", "", ""],
+      ["", "", ""]
+  ];
+
+  const getBoard = () => board;
+
+  const move = (r,p, turn) => {
+    if (board[r][p] === "") {
+      board[r][p] = turn.name;
+    }
+  }
+
+  return {getBoard, move}
+}
+
+function gameControler() {
+  const board = gameBoard();
+
+  const players = [
+    {name: "X", score: 0}, 
+    {name: "Y", score: 0}
+  ]
+  let turn = players[0];
+
+  const addpoint = () => {
+    turn.score += 1;
+  }
+
+  const switchTurn = () => {
+    turn = turn === players[0] ? players[1] : players[0];
+  }
+  const getTurn = () => turn;
+
+  getBoard();
+
+  return {addpoint, switchTurn, getTurn}
+}
+
+function screenControler() {
+  const game = gameControler();
+  const board = gameBoard();
+  const bdiv = document.querySelector(".gboard");
+  const next = document.querySelector(".turn")
+
+  const updateScren = () => {
+    bdiv.textContent = "";
+
+    const brd = board.getBoard();
+    const trn = game.getTurn();
+    next.textContent = `${trn.name}'s turn`
+
+    board.forEach(row => {
+      row.forEach((position, index) => {
+          const btn = document.createElement("button");
+          btn.dataset.r = row;
+          btn.dataset.p = index;
+          bdiv.appendChild(btn);
+      })
     })
-    
-    const render = () => {
-       elem.forEach((el, ind) => {
-        el.textContent = board[ind];
-    })}   
-
-    return {render}
   }
 
-  function gameFlow() {
-    const {getBoard, changeBoard, check} = gameBoard();
-    const {render} = domControl(gameBoard);
-    let turn = "X";
-    let victor = ""
-    let X = 0;
-    let O = 0;
-  
-    const play = (p) => {
-      if (changeBoard(p,turn)) {
-        turn = turn === "X" ? "O" : "X";
-      };
-      render();
-      victor = check();
-      if (victor) {
-        alert(`${turn} wins`);
-        
-    }
-    }
-    
-  
-    return {play}
+  function whenClick(e) {
+    const r = e.target.dataset.r;
+    const p = e.target.dataset.p;
+
+    game.board.move(r,p, game.getTurn());
+    game.switchTurn();
+    updateScren();
   }
+  bdiv.addEventListener("click", whenClick);
 
+  updateScren();
+}
 
-  
-  
-  const game = gameFlow()
-  
-  
-  
+screenControler();
+*/
