74c4a71e1f5d63d8320e68053ef02c4748def29b
emremark <mark.radicc@gmail.com>
2024-12-29 13:39:40 +0100

    Add score display to DOM, add function to update score.

--- script.js
+++ script.js
@@ -34,30 +34,36 @@ const gameC = (function () {
     {name: "X", score: 0}, 
     {name: "O", score: 0}
   ]
+
   let turn = players[0];
 
   const addpoint = () => {
     turn.score += 1;
   }
 
+  const getScoreX = () => players[0].score;
+  const getScoreO = () => players[1].score;
+
   const switchTurn = () => {
     turn = turn === players[0] ? players[1] : players[0];
   }
   const getTurn = () => turn;
 
-  return {addpoint, switchTurn, getTurn}
+  return {addpoint, getScoreX, getScoreO, switchTurn, getTurn}
 })();
 
 function screenControler(gameB,gameC) {
   const bdiv = document.querySelector(".gboard");
   const next = document.querySelector(".turn")
+  let xp = document.querySelector("#xnu");
+  let op = document.querySelector("#onu");
 
   const updateScreen = () => {
     bdiv.textContent = "";
 
     const brd = gameB.getBoard();
     const trn = gameC.getTurn();
-    next.textContent = `${trn.name}'s turn`
+    next.textContent = `${trn.name}'s turn`;
 
     brd.forEach((el, ind) => {
           const btn = document.createElement("button");
@@ -68,11 +74,20 @@ function screenControler(gameB,gameC) {
       })
   }
 
+  const updateScore = () => {
+    xp.textContent = gameC.getScoreX();
+    op.textContent = gameC.getScoreO();
+  }
+
   function whenClick(e) {
     const p = e.target.dataset.p;
     gameB.move(p, gameC.getTurn());
     if (gameB.checkWin(gameB.getBoard(), gameC.getTurn().name)) {
       updateScreen();
+      debugger
+      gameC.addpoint();
+      debugger
+      updateScore();
       alert(`${gameC.getTurn().name} wins!`);
       return
     }
